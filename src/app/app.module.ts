86dd4f5795a7b0e86f0dcfdf5c76964a01fb66a6
Marko Kozjak <kozypato@gmail.com>
2017-12-27 13:13:23 +0100

    directive with Renderer2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import {BasicHighlightDirective} from './basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent, BasicHighlightDirective
+    AppComponent, BasicHighlightDirective, BetterHighlightDirective
   ],
   imports: [
     BrowserModule,
